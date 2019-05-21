cea25079e269ae5a37f0ce909ffb067c7401f2dc
Pierre Martin <hickscorp@gmail.com>
2019-05-21 16:15:26 +0100

    Ignores .elixir_ls, moves to linkedin v2 endpoints.

--- mix.exs
+++ mix.exs
@@ -2,52 +2,42 @@ defmodule UeberauthLinkedin.Mixfile do
   use Mix.Project
 
   @url "https://github.com/fajarmf/ueberauth_linkedin"
+  @description "An Ueberauth strategy for LinkedIn authentication"
 
   def project do
-    [app: :ueberauth_linkedin,
-     version: "0.3.2",
-     name: "Ueberauth LinkedIn Strategy",
-     elixir: "~> 1.2",
-     package: package(),
-     source_url: @url,
-     homepage_url: @url,
-     description: description(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     docs: docs()]
+    [
+      app: :ueberauth_linkedin,
+      version: "0.3.3",
+      name: "Ueberauth LinkedIn Strategy",
+      elixir: "~> 1.2",
+      package: package(),
+      source_url: @url,
+      homepage_url: @url,
+      description: @description,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs()
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
-  def application do
-    [applications: [:logger, :oauth2, :ueberauth]]
-  end
+  def application, do: [applications: [:logger, :oauth2, :ueberauth]]
+  defp docs, do: [extras: docs_extras(), main: "readme"]
+  defp docs_extras, do: ["README.md"]
 
-  defp docs do
-    [extras: docs_extras(), main: "readme"]
-  end
+  defp deps,
+    do: [
+      {:ueberauth, "~> 0.3"},
+      {:oauth2, "~> 2.0"},
+      {:earmark, "~> 1.3", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev}
+    ]
 
-  defp docs_extras do
-    ["README.md"]
-  end
-
-  defp description do
-    "An Ueberauth strategy for LinkedIn authentication"
-  end
-
-  defp deps do
-    [{:ueberauth, "~> 0.3"},
-     {:oauth2, "~> 2.0"},
-     {:earmark, "~> 1.3", only: :dev},
-     {:ex_doc, "~> 0.21", only: :dev}]
-  end
-
-  defp package do
-    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+  defp package,
+    do: [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Fajar Firdaus"],
       licenses: ["MIT"],
-      links: %{"Github": @url}]
-  end
+      links: %{Github: @url}
+    ]
 end
